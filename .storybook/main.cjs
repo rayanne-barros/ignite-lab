710f75726f812464688e7dc03dbb1902bab12001
rayanne-barros <rayanneb126@gmail.com>
2022-10-14 17:58:42 -0300

    Teste no storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -8,14 +8,19 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     '@storybook/addon-a11y',
+    
   ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    "interactionsDebugger": true,
   },
+  "staticDirs": [
+    "../public"
+  ],
   viteFinal: (config, { configType}) => {
     if (configType == 'PRODUCTION') {
       config.base = '/ignite-lab/'
